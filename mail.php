c8ce4824ac23db1a63b7c8ea6323f63cf71c0f43
niklasminth <niklas.minth@tum.de>
2022-06-16 13:06:51 +0200

    images größe angepasst

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "niklas.minth@tum.de";
+$subject = "ITBL Contact-Form";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@green-coding.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
