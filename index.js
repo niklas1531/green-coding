ae2aaf2f89f3cddb4377fc5edc8358f55cb6afa8
niklasminth <niklas.minth@tum.de>
2022-06-16 13:38:09 +0200

    darkmode + contact

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ $('#dark-mode-btn').click(function () {
        
         $(".grey-section").css("backgroundColor", "#474747");
         $(".beige-section").css("backgroundColor", "#403f3d");
+        $(".quiz-container").css("backgroundColor", "#474747");
+        $(".quiz-container").css("box-shadow", "10px 20px 15px #000");
         $(".contact-form").css("box-shadow", "10px 20px 15px #000");
-        $(".about-text-color").css("color", "#000");
+        $(".text-color").css("color", "#000");
         $(".kpi-item").css("box-shadow", "10px 20px 15px #000");
         $(".kpi2-item-box").css("box-shadow", "10px 20px 15px #000");
         $(".team-member").css("box-shadow", "10px 20px 15px #000");
         $("h1").css("color", "#fff");
+        $(".fa-angle-left").css("color", "#fff");
+        $(".fa-angle-right").css("color", "#fff");
+        $(".fa-sitemap").css("color", "#fff");
         $("h2").css("color", "#fff");
         $("h3").css("color", "#fff");
         $("p").css("color", "#fff");
@@ -23,13 +28,18 @@ $('#dark-mode-btn').click(function () {
         $(".kpi-item").css("box-shadow", "10px 20px 15px silver");
         $(".kpi2-item-box").css("box-shadow", "10px 20px 15px silver");
         $(".team-member").css("box-shadow", "10px 20px 15px silver");
+        $(".quiz-container").css("backgroundColor", "#fff");
+        $(".quiz-container").css("box-shadow", "0 0 10px 2px rgba(100, 100, 100, 0.1)");
         $("h1").css("color", "#000");
+        $(".fa-angle-left").css("color", "#000");
+        $(".fa-angle-right").css("color", "#000");
+        $(".fa-sitemap").css("color", "#000");
         $("h2").css("color", "#000");
         $("h3").css("color", "#000");
         $("p").css("color", "#8f8f8f");
         $("li").css("color", "#000");
         $("span").css("color", "#000");
-        $(".about-text-color").css("color", "#000");
+        $(".text-color").css("color", "#000");
         $(".navbar-brand h3").css("color", "#fff");
         $(".heading-1").css("color", "#fff");
     }
