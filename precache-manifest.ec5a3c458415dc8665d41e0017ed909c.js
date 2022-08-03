25d931b9a50e68bf1d9c958a1c70fb5e50d232ee
niklas1531 <97844057+niklas1531@users.noreply.github.com>
2022-08-03 18:02:09 +0200

    Updates

---
+++ precache-manifest.ec5a3c458415dc8665d41e0017ed909c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "76c00dfccad16a08a132",
+    "url": "/green-coding/static/css/main.ebe5da1e.chunk.css"
+  },
+  {
+    "revision": "76c00dfccad16a08a132",
+    "url": "/green-coding/static/js/main.76c00dfc.chunk.js"
+  },
+  {
+    "revision": "e103d14a0b5f7513af18",
+    "url": "/green-coding/static/js/1.e103d14a.chunk.js"
+  },
+  {
+    "revision": "0cb5936727f7d0ce969e",
+    "url": "/green-coding/static/js/runtime~main.0cb59367.js"
+  },
+  {
+    "revision": "85e4fb462c5bee69df738b45460f9822",
+    "url": "/green-coding/static/media/tree.85e4fb46.png"
+  },
+  {
+    "revision": "c76e7a92f2393f8b207b5629a6fcce66",
+    "url": "/green-coding/static/media/earth-image.c76e7a92.png"
+  },
+  {
+    "revision": "98d03fa7baeb4b1924c80d33614b0657",
+    "url": "/green-coding/static/media/quiz-image.98d03fa7.svg"
+  },
+  {
+    "revision": "ec9648ee56710974739b0912ad0c1012",
+    "url": "/green-coding/static/media/logo.ec9648ee.svg"
+  },
+  {
+    "revision": "a0ed4ebeaa0fb6d8b2b26088e8789250",
+    "url": "/green-coding/static/media/kpi-image.a0ed4ebe.jpg"
+  },
+  {
+    "revision": "26921615c6ac2e087743a13310161dd0",
+    "url": "/green-coding/static/media/Alia.26921615.jpg"
+  },
+  {
+    "revision": "fa2596ababb335c0b10938f7ed799201",
+    "url": "/green-coding/static/media/Izzet.fa2596ab.png"
+  },
+  {
+    "revision": "c07f1be858327926db18c62ed44a9f08",
+    "url": "/green-coding/static/media/Marcel.c07f1be8.png"
+  },
+  {
+    "revision": "157ba6043da00adb6536367a5817eb0b",
+    "url": "/green-coding/static/media/Niklas.157ba604.jpeg"
+  },
+  {
+    "revision": "51ecdc40ffcb1bb5eb2833d616d4f223",
+    "url": "/green-coding/static/media/background.51ecdc40.jpeg"
+  },
+  {
+    "revision": "ea53a9c6ace330b439b3d7fa8e956d35",
+    "url": "/green-coding/static/media/iteration:recursion-image.ea53a9c6.jpeg"
+  },
+  {
+    "revision": "7f5718e89200652e4f766404b5945fbd",
+    "url": "/green-coding/static/media/iteration-example.7f5718e8.png"
+  },
+  {
+    "revision": "09a4e99eac90c79c8c704aadc72d336b",
+    "url": "/green-coding/static/media/recursion-example.09a4e99e.png"
+  },
+  {
+    "revision": "859e3d008471ba5d72c41168064ed30c",
+    "url": "/green-coding/index.html"
+  }
+];
